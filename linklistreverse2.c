3c636aea8358d125ac1b193662a0901805bfcf7b
Ashu Rajput <119595975+Ashurajput02@users.noreply.github.com>
2023-05-14 21:16:31 +0530

    Add files via upload

---
+++ linklistreverse2.c
@@ -0,0 +1,70 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+void insert(int a);
+void printer();
+void reverse();
+struct node
+{
+    int data;
+    struct node* link;
+};
+struct node * head;
+
+int main()
+{
+
+head=NULL;//suggests the list is empty so far
+printf("how many numbers\n");
+int n;
+int x;
+scanf("%d",&n);
+for(int i=1;i<=n;i++)
+{
+    printf("enter the number\n");
+    scanf("%d",&x);
+    insert(x);
+   
+}  printer();
+reverse();
+ printer();  
+return 0;
+}
+
+void insert(int a)
+{
+
+    struct node *temp=(struct node *)malloc(sizeof( struct node));//most imp
+    temp->data=a;//the value at the address stored in temp uspe data 
+    temp->link=NULL;//derefrencing pointer variable to store value
+    if(head!=NULL)
+    temp->link=head;/*isse kya hoga yadi a nonempty hoga toh a ka address will be stored in new node and iss new node ka address will be stored in head vioa next line*/
+    head=temp;
+    
+}
+
+ void printer()
+{
+    struct node * temp=head;
+    printf("list is:\n");
+ while(temp!=NULL)
+ {
+    printf("%d",temp->data);
+    temp=temp->link;
+ }
+ printf("\n");
+}
+void reverse()
+{
+    struct node *current,*prev,*next;
+    prev=NULL;
+    current=head;
+    while(current!=NULL)
+    {
+        next=current->link;
+        current->link=prev;
+        prev=current;
+        current=next;
+    }
+    head=prev;
+}
